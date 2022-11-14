618a9135348f9641bf9a998013fdd4396697acad
wahyuningrat005 <wahyu09hd@gmail.com>
2022-11-14 16:04:22 +0800

    splash file

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,28 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+              child: Text(
+            "Furniture Shop",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+          ))
+        ],
+      ),
+    );
+  }
+}
